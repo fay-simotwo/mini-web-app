126a5f40a6c58d60583f4255427331d385570c30
Moringa School <chepfaith18@gmail.com>
2023-06-26 00:05:33 +0300

    assigning html elements to the variables

---
+++ index.js
@@ -0,0 +1,5 @@
+// Get references to HTML elements
+let animalList = document.getElementById('animal-list');
+let animalDetails = document.getElementById('animal-details');
+let resetButton = document.getElementById('reset-button');
+let currentAnimal = null;
