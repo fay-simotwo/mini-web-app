bfc592479b7a6124183e16fb50889d5c28074442
Moringa School <chepfaith18@gmail.com>
2023-06-26 00:08:46 +0300

    fetching data from the server

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let animalList = document.getElementById('animal-list');
 let animalDetails = document.getElementById('animal-details');
 let resetButton = document.getElementById('reset-button');
 let currentAnimal = null;
+
+// Fetch data from the server and populate the animal list
+fetch('http://localhost:3000/characters')
+  .then(response => response.json())
+  .then(characters => {
+    for (const character of characters) 
